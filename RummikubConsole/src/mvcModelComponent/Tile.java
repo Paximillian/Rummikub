0dfdb07a1a801c042a105432f22f58846346376a
eitanacton <eitanacton@gmail.com>
2015-11-12 15:18:21 +0200

    f

--- RummikubConsole/src/mvcModelComponent/Tile.java
+++ RummikubConsole/src/mvcModelComponent/Tile.java
@@ -27,6 +27,39 @@ public class Tile {
             return valueOf(v);
         }           
     }
-    private int value;
-    private Color color; 
+    
+    public enum Rank{
+        
+        ONE(1),
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        ELEVEN(11),
+        TWELVE(12),
+        THIRTEEN(13);
+        
+        private final int value;
+        
+        private  Rank(int value){
+            this.value = value;
+        }
+        public int value(){
+            return this.value;
+        }
+        
+    }
+    public Tile(Color color, Rank value)
+    {
+        this.color = color;
+        this.value = value;
+    }
+    private final Rank value;
+    private final Color color; 
+    
 }
