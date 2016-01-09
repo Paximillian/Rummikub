8e7538079a89477531e9c5ce94e2c0b8b78977f1
Paximillian <morzil@walla.com>
2016-01-09 04:35:57 +0200

    Remade server project

--- RummikubWebServer/src/java/ws/server/RummiWS.java
+++ RummikubWebServer/src/java/ws/server/RummiWS.java
@@ -3,10 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ws.game;
+package ws.server;
 
-import java.util.ArrayList;
 import javax.jws.WebService;
+import ws.rummikub.DuplicateGameName_Exception;
+import ws.rummikub.GameDoesNotExists_Exception;
+import ws.rummikub.InvalidParameters_Exception;
+import ws.rummikub.InvalidXML_Exception;
 
 /**
  *
@@ -15,72 +18,72 @@ import javax.jws.WebService;
 @WebService(serviceName = "RummikubWebServiceService", portName = "RummikubWebServicePort", endpointInterface = "ws.rummikub.RummikubWebService", targetNamespace = "http://rummikub.ws/", wsdlLocation = "WEB-INF/wsdl/RummiWS/RummikubWebServiceService.wsdl")
 public class RummiWS {
 
-    public java.util.List<ws.rummikub.Event> getEvents(int playerId, int eventId) throws ws.rummikub.InvalidParameters_Exception {
+    public java.util.List<ws.rummikub.Event> getEvents(int playerId, int eventId) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public java.lang.String createGameFromXML(java.lang.String xmlData) throws ws.rummikub.InvalidXML_Exception, ws.rummikub.DuplicateGameName_Exception, ws.rummikub.InvalidParameters_Exception {
+    public java.lang.String createGameFromXML(java.lang.String xmlData) throws InvalidParameters_Exception, DuplicateGameName_Exception, InvalidXML_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public java.util.List<ws.rummikub.PlayerDetails> getPlayersDetails(java.lang.String gameName) throws ws.rummikub.GameDoesNotExists_Exception {
+    public java.util.List<ws.rummikub.PlayerDetails> getPlayersDetails(java.lang.String gameName) throws GameDoesNotExists_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void createGame(java.lang.String name, int humanPlayers, int computerizedPlayers) throws ws.rummikub.InvalidParameters_Exception, ws.rummikub.DuplicateGameName_Exception {
+    public void createGame(java.lang.String name, int humanPlayers, int computerizedPlayers) throws InvalidParameters_Exception, DuplicateGameName_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public ws.rummikub.GameDetails getGameDetails(java.lang.String gameName) throws ws.rummikub.GameDoesNotExists_Exception {
+    public ws.rummikub.GameDetails getGameDetails(java.lang.String gameName) throws GameDoesNotExists_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     public java.util.List<java.lang.String> getWaitingGames() {
         //TODO implement this method
-        return new ArrayList<String>(){{add("test");}};
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public int joinGame(java.lang.String gameName, java.lang.String playerName) throws ws.rummikub.InvalidParameters_Exception, ws.rummikub.GameDoesNotExists_Exception {
+    public int joinGame(java.lang.String gameName, java.lang.String playerName) throws GameDoesNotExists_Exception, InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public ws.rummikub.PlayerDetails getPlayerDetails(int playerId) throws ws.rummikub.GameDoesNotExists_Exception, ws.rummikub.InvalidParameters_Exception {
+    public ws.rummikub.PlayerDetails getPlayerDetails(int playerId) throws GameDoesNotExists_Exception, InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void createSequence(int playerId, java.util.List<ws.rummikub.Tile> tiles) throws ws.rummikub.InvalidParameters_Exception {
+    public void createSequence(int playerId, java.util.List<ws.rummikub.Tile> tiles) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void addTile(int playerId, ws.rummikub.Tile tile, int sequenceIndex, int sequencePosition) throws ws.rummikub.InvalidParameters_Exception {
+    public void addTile(int playerId, ws.rummikub.Tile tile, int sequenceIndex, int sequencePosition) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void takeBackTile(int playerId, int sequenceIndex, int sequencePosition) throws ws.rummikub.InvalidParameters_Exception {
+    public void takeBackTile(int playerId, int sequenceIndex, int sequencePosition) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void moveTile(int playerId, int sourceSequenceIndex, int sourceSequencePosition, int targetSequenceIndex, int targetSequencePosition) throws ws.rummikub.InvalidParameters_Exception {
+    public void moveTile(int playerId, int sourceSequenceIndex, int sourceSequencePosition, int targetSequenceIndex, int targetSequencePosition) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void finishTurn(int playerId) throws ws.rummikub.InvalidParameters_Exception {
+    public void finishTurn(int playerId) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
-    public void resign(int playerId) throws ws.rummikub.InvalidParameters_Exception {
+    public void resign(int playerId) throws InvalidParameters_Exception {
         //TODO implement this method
         throw new UnsupportedOperationException("Not implemented yet.");
     }
