fac830c4cece8923e55ab290160fcb3704a6aaef
Paximillian <morzil@walla.com>
2016-01-09 04:28:52 +0200

    Revert "Created web project, started porting gui client to web client"
    This reverts commit 54e6b870f752de3cae4600b2676b6edc3adafaf2.

--- RummikubGUI/src/mvcViewComponent/gui/mainMenu/MainMenu.java
+++ RummikubGUI/src/mvcViewComponent/gui/mainMenu/MainMenu.java
@@ -6,15 +6,12 @@
  */
 package mvcViewComponent.gui.mainMenu;
 
-import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import mvcControllerComponent.client.ws.RummikubWebService;
-import mvcControllerComponent.client.ws.RummikubWebServiceService;
 import mvcViewComponent.gui.sceneController.ScreensController;
 
 /**
@@ -32,7 +29,7 @@ public class MainMenu extends Application {
        Group root = new Group(); 
        root.getChildren().addAll(ScreensController.getInstance()); 
        Scene scene = new Scene(root, 800, 650); 
-              
+       
        stage.setScene(scene); 
        ScreensController.getInstance().setScreen(ScreensController.MAIN_SCENE);
        stage.setResizable(false);
